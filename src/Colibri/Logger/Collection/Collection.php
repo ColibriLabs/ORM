92bef254831ad8a11c2b2b358c1ad1c068b809eb
Ivan Hontarenko <dezbyte@gmail.com>
2017-05-23 12:41:39 +0300

    Merge

--- src/Colibri/Logger/Collection/Collection.php
+++ src/Colibri/Logger/Collection/Collection.php
@@ -2,12 +2,12 @@
 
 namespace Colibri\Logger\Collection;
 
-use Colibri\Collection;
+use Colibri\Collection\Collection as BaseCollection;
 
 /**
  * Class ArrayCollection
  * @package Colibri\Logger\Collection
  */
-class Collection extends Collection\Collection
+class Collection extends BaseCollection
 {
 }
