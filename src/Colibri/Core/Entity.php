eb88c51a0adfed47580fb2e0927364491c6fac1c
Ivan Hontarenko <dezbyte@users.noreply.github.com>
2017-06-20 17:57:01 +0300

    Update Entity.php

--- src/Colibri/Core/Entity.php
+++ src/Colibri/Core/Entity.php
@@ -63,7 +63,7 @@ abstract class Entity implements EntityInterface
    */
   public function hasProperty($propertyName)
   {
-    return return property_exists($this, $propertyName);
+    return property_exists($this, $propertyName);
   }
 
   /**
