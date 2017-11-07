b6c844f1411d32e0a26cd7c39909f7a117e676be
Ivan Hontarenko <dezbyte@users.noreply.github.com>
2017-11-07 17:32:15 +0200

    Update GroupConcat.php

--- src/Colibri/Query/Expr/Func/GroupConcat.php
+++ src/Colibri/Query/Expr/Func/GroupConcat.php
@@ -12,12 +12,78 @@ class GroupConcat extends Func
 {
   
   /**
+   * @var string
+   */
+  private $separator;
+  
+  /**
+   * @var string
+   */
+  private $orderVector;
+  
+  /**
    * GroupConcat constructor.
    * @param array ...$parameters
    */
   public function __construct(...$parameters)
   {
     parent::__construct('GROUP_CONCAT', $parameters);
+  }
+  
+  /**
+   * @return string
+   */
+  public function getSeparator()
+  {
+    return $this->separator;
+  }
+  
+  /**
+   * @param string $separator
+   * @return $this
+   */
+  public function setSeparator($separator)
+  {
+    $this->separator = $separator;
+    
+    return $this;
+  }
+  
+  /**
+   * @return string
+   */
+  public function getOrderVector()
+  {
+    return $this->orderVector;
+  }
+  
+  /**
+   * @param string $orderVector
+   * @return $this
+   */
+  public function setOrderVector($orderVector)
+  {
+    $this->orderVector = $orderVector;
+  
+    return $this;
+  }
+  
+  /**
+   * @inheritdoc
+   */
+  protected function toStringFunctionArguments()
+  {
+    $arguments = parent::toStringFunctionArguments();
+  
+    if (null !== $this->getSeparator()) {
+      $arguments = sprintf("%s SEPARATOR '%s'", $arguments, $this->getSeparator());
+    }
+  
+    if (null !== $this->getOrderVector()) {
+      $arguments = sprintf("%s ORDER BY %s", $arguments, $this->getOrderVector());
+    }
+    
+    return $arguments;
   }
   
 }
