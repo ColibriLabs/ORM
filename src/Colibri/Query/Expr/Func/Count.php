09ba558755f1d99996b00f721e27a8ad2369dfe0
Ivan Hontarenko <dezbyte@users.noreply.github.com>
2017-11-07 17:49:23 +0200

    Update Count.php

--- src/Colibri/Query/Expr/Func/Count.php
+++ src/Colibri/Query/Expr/Func/Count.php
@@ -5,19 +5,61 @@ namespace Colibri\Query\Expr\Func;
 use Colibri\Query\Expr\Func;
 use Colibri\Exception\BadCallMethodException;
 
+/**
+ * Class Count
+ * @package Colibri\Query\Expr\Func
+ */
 class Count extends Func
 {
-
+  
+  private $isDistinct = false;
+  
   /**
    * Count constructor.
    * MySQL Function COUNT
    *
-   * @param array $parameters
    * @throws BadCallMethodException
+   * @param string $columnName
+   * @param bool $isDistinct
    */
-  public function __construct(...$parameters)
+  public function __construct($columnName, $isDistinct = false)
   {
-    parent::__construct('COUNT', $parameters);
+    parent::__construct('COUNT', [$columnName]);
+    
+    $this->setDistinct($isDistinct);
+  }
+  
+  /**
+   * @return bool
+   */
+  public function isDistinct()
+  {
+    return $this->isDistinct;
+  }
+  
+  /**
+   * @param bool $isDistinct
+   * @return $this
+   */
+  public function setDistinct($isDistinct)
+  {
+    $this->isDistinct = (boolean)$isDistinct;
+    
+    return $this;
+  }
+  
+  /**
+   * @inheritDoc
+   */
+  protected function toStringFunctionArguments()
+  {
+    $arguments = parent::toStringFunctionArguments();
+    
+    if (true === $this->isDistinct()) {
+      $arguments = sprintf('DISTINCT %s', $arguments);
+    }
+    
+    return $arguments;
   }
   
 }
